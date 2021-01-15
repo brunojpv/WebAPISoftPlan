92cc333e515f710b49c17daeb27d9ddd05ee2d4b
brunojpv <brunojpv@gmail.com>
2021-01-15 01:04:08 -0300

    Adicionando Logs

--- API2/Program.cs
+++ API2/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Reflection;
 
 namespace API2
 {
@@ -7,11 +12,53 @@ namespace API2
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IConfigurationRoot configuration = GetConfiguration();
+            ConfiguraLog(configuration);
+
+            try
+            {
+                Log.Information("Iniciando worker!!!");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Erro no worker!!!");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void ConfiguraLog(IConfigurationRoot configuration)
+        {
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+            Log.Logger = new LoggerConfiguration()
+                            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+                            {
+                                AutoRegisterTemplate = true,
+                                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
+                            })
+                            .Enrich.WithProperty("Environment", environment)
+                            .ReadFrom.Configuration(configuration)
+                            .CreateLogger();
+        }
+
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .Build();
+
+            return configuration;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
